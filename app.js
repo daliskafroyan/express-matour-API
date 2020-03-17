0d22c598cd9a306ae2d851ef887b9e961e72bdf1
Daliska F. Royan <18845506030@163.com>
2020-03-17 22:01:32 +0700

    add GET route to fetch all and individual tours data

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const fs = require("fs");
 
 const app = express();
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours.json`));
+app.use(express.json());
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/tours.json`)
+);
 
 app.get("/api/v1/tours", (req, res) => {
     res.status(200).json({
@@ -15,8 +19,44 @@ app.get("/api/v1/tours", (req, res) => {
     })
 });
 
-app.post("/", (req, res) => {
-  res.send("you post here babe");
+app.get("/api/v1/tours/:id", (req,res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find( el => el.id === id);
+
+    if(!tour) {
+        return res.status(404).json({
+            status: "failed",
+            message: "invalid id"
+        })
+    }
+
+    res.status(200).json({
+        status : "success",
+        data : {
+            tour
+        }
+    });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign( {id : newId}, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(
+        `${__dirname}/data/tours.json`, 
+        JSON.stringify(tours), 
+        err => {
+        res.status(201).json({
+             status: "success",
+             data : {
+                 tour : newTour
+             }
+        });
+    }
+    );
 });
 
 const port = 3000;
